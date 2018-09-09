90a5911c4cf21f8327ba1b02c9911f2deab49789
grizzthepro64 <42747215+grizzthepro64@users.noreply.github.com>
2018-09-09 09:18:26 -0400

    Final push for R code
    R code with functions defined, using glm

--- Case 1.R
+++ Case 1.R
@@ -1,55 +1,58 @@
-#Exploratory
-
-#Load packages
-library(pacman)
-p_load(tidyverse)
-p_load(dplyr)
-p_load(stringr)
-p_load(randomForest)
-p_load(AUC)
-p_load(lift)
-
 #Read in data
 customers <- read_csv("customers.csv")
 purchases <- read_csv("purchases.csv")
 registrations <- read_csv("registrations.csv")
 
-#Customers not included in registrations
-customers <- customers[customers$CompanyName %in% registrations$CompanyName,] #make sure all customers are registered
-purchases <- purchases[purchases$CustomerID %in% customers$CustomerID,] #Make sure all purchases are part of customers
 
-#Does purchases = registered users
-regsmall <- 
-  registrations %>% 
-  select(CompanyName, CompanyAddress, RegistrationDate) %>% 
-  group_by(CompanyName, CompanyAddress) %>%
-  summarise(NumRegistered = n(), FirstRegDate = min(RegistrationDate), LastRegDate = max(RegistrationDate)) #Find #, first and last date for each co.
+fitModel <- function(customers, purchases, registrations){
+  #Load packages
+  library(pacman)
+  p_load(tidyverse)
+  p_load(dplyr)
+  p_load(stringr)
+  p_load(randomForest)
+  p_load(AUC)
+  p_load(lift)
+  p_load(lubridate)
+  p_load(zipcode)
+  
+  data("zipcode") #To match state to zip
+  
+  #Customers not included in registrations
+  customers <- customers[customers$CompanyName %in% registrations$CompanyName,] #make sure all customers are registered
+  purchases <- purchases[purchases$CustomerID %in% customers$CustomerID,] #Make sure all purchases are part of customers
+  
+  #Does purchases = registered users
+  regsmall <- 
+    registrations %>% 
+    select(CompanyName, CompanyAddress, RegistrationDate) %>% 
+    group_by(CompanyName, CompanyAddress) %>%
+    summarise(NumRegistered = n(), FirstRegDate = min(RegistrationDate), LastRegDate = max(RegistrationDate)) #Find #, first and last date for each co.
+  
+  #Feature ID/Engineering
+  regsmall$LastRegLength <- as.integer(as_date("2016-01-01") - regsmall$LastRegDate) #Difference in last registration date & first day of 2016
+  regsmall$FirstRegLength <- as.integer(as_date("2016-01-01") - regsmall$FirstRegDate) #Difference in first registration date & first day of 2016
+  regsmall$zip <- str_extract(regsmall$CompanyAddress, "\\d{5}")#Extract Zip code
+  regsmall <- regsmall %>% left_join(zipcode %>% select(zip, state)) #Get state from zip
+  regsmall$state <- as.factor(regsmall$state) #Convert to factor
+  regsmall <- regsmall[complete.cases(regsmall$state),] #Remove NAs
+  regsmall$FirstMonth <- as.factor(month(regsmall$FirstRegDate))
+  regsmall$LastMonth <- as.factor(month(regsmall$LastRegDate))
+  
+  #did customer purchase or not?
+  regsmall$Converted <- as.factor(as.numeric(regsmall$CompanyName %in% customers$CompanyName)) #Create response
+  
+  
+  #Fit Model
+  glmodel <- glm(Converted ~  LastRegLength + FirstRegLength + FirstMonth + LastMonth + NumRegistered + state, regsmall, family = "binomial")
+  return(glmodel)
+}
 
+predictCust <- function(glmodel, newdata){
+  gpred <- predict(glmodel, newdata, type = "response")
+  return(gpred)
+}
 
-#Feature ID/Engineering
-
-#Create a "lifetime" variable for each customer that takes sysdate - date of first registration for that customer
-regsmall$Lifetime <- Sys.Date() - regsmall$FirstRegDate #Difference in first registration date & current date
-regsmall$LastRegLength <- Sys.Date() - regsmall$LastRegDate #Difference in last registration date & current date
-regsmall$zip <- str_extract(regsmall$CompanyAddress, "\\d{5}") #Extract Zip code
-
-#did customer purchase or not?
-regsmall$Converted <- as.numeric(regsmall$CompanyName %in% customers$CompanyName) 
-regsmall$Converted <- as.factor(regsmall$Converted) #create binary conversion variable
-
-
-#Build Model?
-trows <- sample(1:nrow(regsmall), .2*nrow(regsmall))
-ctrain <- regsmall[-trows,]
-
-ctest <- regsmall[trows,]
-
-
-
-rf <- randomForest(Converted ~ NumRegistered + Lifetime + LastRegLength + zip, ctrain)
-pred <- predict(rf, ctest)
-auc(roc(pred, ctest$Converted))
-TopDecileLift(pred, ctest$Converted)
 
 
 
